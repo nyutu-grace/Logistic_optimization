71ea42892d01c65f9694de5ba9b9945a8c01e592
nyutu_grace <nyutu_grace15@gmail.com>
2024-06-15 21:10:18 +0100

    add a causal analysis script

---
+++ scripts/causal_inference.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import numpy as np
+from dowhy import CausalModel
+
+def perform_causal_analysis(train_df):
+    # Define interventions
+    interventions = {
+        'driver_movement': 1,
+        'order_location_accuracy': 5,
+        'driver_operating_time': 8,
+        'num_drivers': 10
+    }
+
+    results = {}
+
+    for treatment in interventions.keys():
+        model = CausalModel(
+            data=train_df,
+            treatment=[treatment],
+            outcome='unfulfilled_requests',
+            common_causes=['hour', 'day_of_week', 'is_weekend', 'driver_id', 'duration_hours', 'holiday']
+        )
+
+        identified_estimand = model.identify_effect(proceed_when_unidentifiable=True)
+
+        causal_estimate = model.estimate_effect(
+            identified_estimand,
+            method_name="backdoor.linear_regression"
+        )
+
+        results[treatment] = causal_estimate.value
+
+    return results
